2461433fff55c8bbe44ffc6593401c65c8689084
Richard de Vries <Pruyque@outlook.com>
2025-07-30 22:14:21 +0200

    Player detector

---
+++ Assets/Scripts/Level Elements/PlayerDetector.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class PlayerDetector : MonoBehaviour
+{
+    int player_count = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public UnityEvent playerEnter;
+    public UnityEvent playerExit;
+
+    void Start()
+    {
+        
+    }
+
+	private void OnCollisionEnter2D(Collision2D collision)
+	{
+        if (null != collision.gameObject.GetComponent<MyInputUser>())
+        {
+            if (0 == player_count)
+			    playerEnter.Invoke();
+			player_count++;
+        }
+	}
+	private void OnCollisionExit2D(Collision2D collision)
+	{
+        if (null != collision.gameObject.GetComponent<MyInputUser>())
+        {
+            if (player_count > 0)
+            {
+                player_count--;
+                if (0 == player_count)
+                    playerExit.Invoke();
+            }
+        }
+	}
+    public float compression_time = 1.0f;
+    public float compression = 1.0f;
+	// Update is called once per frame
+	void Update()
+    {
+        if (player_count > 0 && compression > 0)
+        {
+            compression = Mathf.Max(0, compression - Time.deltaTime / compression_time);
+        }
+        if (player_count == 0 && compression < 1)
+        {
+            compression = Mathf.Min(1, compression + Time.deltaTime / compression_time);
+        }
+    }
+}
