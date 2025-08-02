23019123220d970157463f0233795e1860980533
Richard de Vries <Pruyque@outlook.com>
2025-08-02 20:13:09 +0200

    Properly lined

--- Assets/Scripts/Level Elements/ExitDoor.cs
+++ Assets/Scripts/Level Elements/ExitDoor.cs
@@ -5,6 +5,7 @@ using UnityEngine.SceneManagement;
 public class ExitDoor : MonoBehaviour
 {
     [SerializeField] private string nextLevelName;
+    public GameObject TransitionFocus;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         FindAnyObjectByType<MyInputManager>().GoalReached();
